0cf6bcc7d8043743aa0f6437352fbcb97d19acd9
benoitbds <32876581+benoitbds@users.noreply.github.com>
2025-06-14 07:07:02 +0200

    Update main.py
    debug startup

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,10 +20,13 @@ app.add_middleware(
 
 @app.on_event("startup")
 def on_startup():
+    printf('startup function')
     if not settings.openai_api_key:
         raise ValueError("OPENAI_API_KEY is missing")
+    else printf('OPENAI_API_KEY set !')
     if settings.secret_key == "CHANGE_ME":
         raise RuntimeError("Please set SECRET_KEY in backend/.env")
+    else printf('SECRET set')
     init_db()
 
 app.include_router(auth.router)
